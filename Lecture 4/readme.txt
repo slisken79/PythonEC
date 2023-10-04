ec78900bcd4eea522c0d8dd337692f309743a94f
Jonas Jons <jonas.jons@consid.se>
2023-10-04 14:04:34 +0200

    added baseline lecture 4

---
+++ Lecture 4/readme.txt
@@ -0,0 +1,1 @@
+# To start up the server run the command npx http-server, package might be required
---
+++ Lecture 4/script.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('main');
+const button = document.createElement('button');
+button.textContent = 'Change Color';
+button.addEventListener('click', () => {
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    main.style.backgroundColor = randomColor;
+});
+main.appendChild(button);
