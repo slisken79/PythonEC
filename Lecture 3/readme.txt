ec37833d1dcf3283ef218466392ba06b3b27a033
Jonas Jons <jonas.jons@consid.se>
2023-10-05 11:46:39 +0200

    added the missing files, init still not working

---
+++ Lecture 3/readme.txt
@@ -0,0 +1,3 @@
+To install all the packages, run the "pip install -r requirements.txt"
+
+To save your current packages and libraries you use, run "pip freeze > requirements.txt"
---
+++ Lecture 3/testdir/my_person.py
@@ -0,0 +1,22 @@
+
+CITY_OF_ORIGIN = "Uppsala"
+
+
+class Person():
+    def __init__(self, age=10, name="John"):
+        super().__init__()
+        self.__age = age
+        self.name = name
+        self.__city = CITY_OF_ORIGIN
+        print(self.name)
+
+    def get_person_age(self):
+        """Gets the age of the person"""
+        return self.__age
+
+    def print_person_age(self):
+        print(self.__age)
+
+
+my_person = Person()
+my_person.print_person_age()
